076461bc8ff0e642e1e350ef6858191c1ddc6c85
jinho <dodwn48@gmail.com>
2025-01-21 15:47:59 +0900

    t

--- tutorial_sp/test.py
+++ tutorial_sp/test.py
@@ -1,3 +1,18 @@
-while True:
-    A, B = map(int, input().split())
-    print(A + B)
+import sys
+
+N = 28
+list_s = [0 for i in range(N)]
+mc = 1
+
+for i in range(N):
+    list_s[i] = int(sys.stdin.readline())
+
+list_s.sort()
+
+for i in range(N):
+    if mc != list_s[i]:
+        print(mc)
+        mc += 1
+    mc += 1
+
+#29 30 일때 출력안함
