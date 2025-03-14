8f255358cda3904fb04ab4b6079abcfa0da2c47a
Jongseok Baek <baekjs0123@naver.com>
2025-03-14 14:18:24 +0900

    14

--- study/week_06/10942.py
+++ study/week_06/10942.py
@@ -4,16 +4,16 @@ input = lambda:sys.stdin.readline().strip()
 print = lambda x:sys.stdout.write(x)
 
 
-def main():
-    N = int(input())
-    arr = tuple(map(int, input().split()))
+N = int(input())
+arr = tuple(map(int, input().split()))
 
-    dp = [[0] * N for _ in range(N)]
+dp = [[0] * N for _ in range(N)]
 
-    for i in range(N):
-        for j in range(i, N):
-            dp[i]
-            
+for i in range(N):
+    dp[i][i] = 1
 
-    M = int(input())
-    for _ in range(M):
+
+
+
+M = int(input())
+for _ in range(M):
