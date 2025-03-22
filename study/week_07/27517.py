e1f8605a197e4083a51582c71d687243904230ce
PPPP98 <dodwn48@gmail.com>
2025-03-22 15:09:14 +0900

    test

---
+++ study/week_07/27517.py
@@ -0,0 +1,23 @@
+import sys
+
+input = lambda:sys.stdin.readline().strip()
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+sort_arr = sorted(arr)
+res = 0
+
+for i in range(N - 1, -1, -1):
+    target = sort_arr[i]
+    j = 0
+    now = 0
+    while now != target:
+        if now < arr[i - j]:
+            remain = arr[i - j] - now
+            now += remain
+            arr[i - j] -= remain
+            res += (j * remain)
+        j += 1
+
+print(res)
