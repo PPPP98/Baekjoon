5fba05ed6f6ac148a9cc28139bb1d9bb1008828d
김수민 <gkgkgkgk22@naver.com>
2025-04-01 13:43:57 +0900

    01

--- class_04/15666.py
+++ class_04/15666.py
@@ -1,10 +1,3 @@
-"""
-N개의 자연수 중에서 M개를 고른 수열
-같은 수를 여러 번 골라도 된다.
-고른 수열은 비내림차순이어야 한다.
-길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
-"""
-
 import sys
 
 input = lambda: sys.stdin.readline().strip()
